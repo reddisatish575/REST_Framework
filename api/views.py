3e79b7c6ffa184c6debf0d92d2e649321c385a5f
reddisatish575 <reddisatish575@gmail.com>
2025-11-29 21:54:09 +0530

    generics

--- api/views.py
+++ api/views.py
@@ -57,44 +57,47 @@ def studentDetailView(request, pk):
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-# Class-Based Views (it should inherit APIView)
-# class Employees(APIView):
-#     def get(self, request):
-#         employees = Employee.objects.all()
-#         serializer = EmployeeSerializer(employees, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#     def post(self, request):
-#         serializer = EmployeeSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+''' # Class-Based Views (it should inherit APIView)
+class Employees(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-# class EmployeeDetail(APIView):
-#     def get_object(self,pk):
-#         try:
-#             employee = Employee.objects.get(pk=pk)
-#             return employee
-#         except Employee.DoesNotExist:
-#             raise Http404
+class EmployeeDetail(APIView):
+    def get_object(self,pk):
+        try:
+            employee = Employee.objects.get(pk=pk)
+            return employee
+        except Employee.DoesNotExist:
+            raise Http404
     
-#     def get(self,request, pk):
-#         employee = self.get_object(pk)
-#         serializer = EmployeeSerializer(employee)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self,request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
-#     def put(self,request,pk):
-#         employee = self.get_object(pk)
-#         serializer = EmployeeSerializer(employee, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self,request,pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def delete(self,request, pk):
-#         employee = self.get_object(pk)
-#         employee.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self,request, pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
+'''
+
+''' # Using Mixins and GenericAPIView 
 
 class Employees(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Employee.objects.all()
@@ -119,3 +122,14 @@ class EmployeeDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.
     def delete(self,request, pk):
         return self.destroy(request, pk)    
 
+'''
+
+# Using Generic Class-Based Views
+class Employees(generics.ListAPIView, generics.CreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class EmployeeDetail(generics.RetrieveAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    lookup_field = 'pk'
