637f44f7d0ca4c7f9d8570c19ecd40f66503c5be
reddisatish575 <reddisatish575@gmail.com>
2025-12-01 23:01:46 +0530

    url changes

--- api/urls.py
+++ api/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('comments/', views.CommentsView.as_view()),
 
     path('blogs/<int:pk>/', views.BlogDetailView.as_view()),
-    path('comments/<int:pk>/', views.CommentDetailView.as_view()),
+    path('comments/<int:pk>/', views.CommentDetailView.as_view())
 
 ]
